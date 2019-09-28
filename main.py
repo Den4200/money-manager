aee854ff987eff93a3df715a774d9dfe87d985cd
Den4200 <dpham.42@hotmail.com>
2019-09-28 18:27:18 -0400

    added banks

--- main.py
+++ main.py
@@ -1,11 +1,69 @@
 import os
 import sys
+import csv
+import shutil
 import time
 
-class Manager:
+class User:
+    pass
+
+class BankManager(User):
     
     def __init__(self):
-        self.file = os.path.join(sys.path[0], "money.csv")
+        self.b_file = os.path.join(sys.path[0], "banks\\banks.csv")
+        self.sl_file = os.path.join(sys.path[0], "banks\\session.lock")
+
+    def listBanks(self):
+        banks = []
+
+        with open(self.b_file, 'r') as f:
+            lines = f.readlines()
+
+            for line in lines:
+                banks.append(line[:-1])
+
+        return banks
+
+    def addBank(self, bank):
+        os.mkdir('banks\\'+ bank)
+
+        with open(self.b_file, 'a') as f:
+            f.write(bank + '\n')
+
+        with open('banks\\' + bank + '\\' 'transactions.csv', 'w') as f:
+            f.write('Initialization,initial,+0.0,0.0\n')
+
+    def delBank(self, bank):
+        read = open(self.b_file, 'r')
+        f = open(os.path.join(sys.path[0], "banks\\banks_temp.csv"), 'w')
+
+        reader = read.readlines()
+
+        for line in reader:
+            if bank != line[:-1]:
+                f.write(line)
+
+        read.close()
+        f.close()
+
+        os.remove(self.b_file)
+        os.rename(os.path.join(sys.path[0], "banks\\banks_temp.csv"), os.path.join(sys.path[0], "banks\\banks.csv"))
+
+        shutil.rmtree(os.path.join(sys.path[0], "banks\\" + bank))
+
+    def chooseBank(self, bank):
+        with open(self.sl_file, 'w') as f:
+            f.write('')
+            f.write(bank)
+
+    def _returnCurrent(self):
+        with open(self.sl_file, 'r') as f:
+            return f.read()
+
+class TransactionsManager(BankManager):
+
+    def __init__(self):
+        self.t_file = os.path.join(sys.path[0], "banks\\" + BankManager()._returnCurrent() + "\\transactions.csv")
 
     def _store(self, op, amount, total):
 
@@ -14,11 +72,11 @@ class Manager:
         elif op == 'withdraw':
             sym = '-'
 
-        with open(self.file, 'a') as f:
+        with open(self.t_file, 'a') as f:
             f.write(f'{time.ctime()},{op},{sym}{amount},{total}\n')
 
     def _read(self):
-        with open(self.file, 'r') as f:
+        with open(self.t_file, 'r') as f:
             line = f.readlines()[-1].split(',')
         
         date = line[0]
@@ -62,7 +120,7 @@ class Manager:
     def listTransactions(self):
         transactions = []
 
-        with open(self.file, 'r') as f:
+        with open(self.t_file, 'r') as f:
             lines = f.readlines()
 
             for line in lines:
@@ -87,43 +145,88 @@ class Manager:
 def mainLoop():
 
     while True:
-        option = input("""
+
+        bank_op = input("""
         --------------------------
-        | 1. Deposit             |
-        | 2. Withdraw            |
-        | 3. List transactions   |
-        | 4. Exit                |
+        | 1. Choose bank         |
+        | 2. Add bank            |
+        | 3. Remove Bank         |
+        | 4. List banks          |
+        | 5. Exit                |
         --------------------------
 
         Option: """)
 
-        if option == '1':
-            a = input('\nAmount to deposit: ')
+        if bank_op == '1':
+            for bank in BankManager().listBanks():
+                print(bank)
+            
+            bank = input('\nBank (Type in exactly as shown): ')
 
-            if Manager().deposit(a) == 'not_number':
-                print('\nDeposit failed.. Value entered was not a number.')
+            BankManager().chooseBank(bank)
 
-        elif option == '2':
-            a = input('\nAmount to withdraw: ')
-            mw = Manager().withdraw(a)
 
-            if mw == 'not_enough_money':
-                print('\nWithdraw failed.. There is not enough money in your account.')
-            elif mw == 'not_number':
-                print('\nWithdraw failed.. Value entered was not a number.')
+            option = input("""
+            --------------------------
+            | 1. Deposit             |
+            | 2. Withdraw            |
+            | 3. List transactions   |
+            | 4. Exit                |
+            --------------------------
 
-        elif option == '3':
-            for transaction in Manager().listTransactions():
-                print(f'\nDate: {transaction[0]}\n{transaction[1].capitalize()}: ${transaction[2][1:]}\nTotal: ${transaction[3]}\n')
+            Option: """)
 
-        elif option == '4':
+            if option == '1':
+                a = input('\nAmount to deposit: ')
+
+                if TransactionsManager().deposit(a) == 'not_number':
+                    print('\nDeposit failed.. Value entered was not a number.')
+
+            elif option == '2':
+                a = input('\nAmount to withdraw: ')
+                mw = TransactionsManager().withdraw(a)
+
+                if mw == 'not_enough_money':
+                    print('\nWithdraw failed.. There is not enough money in your account.')
+                elif mw == 'not_number':
+                    print('\nWithdraw failed.. Value entered was not a number.')
+
+            elif option == '3':
+                for transaction in TransactionsManager().listTransactions():
+                    print(f'\nDate: {transaction[0]}\n{transaction[1].capitalize()}: ${transaction[2][1:]}\nTotal: ${transaction[3]}\n')
+
+            elif option == '4':
+                break
+
+            else:
+                print('\nOption is not valid!')
+
+        elif bank_op == '2':
+            new_bank = input('Bank name: ')
+            BankManager().addBank(new_bank)
+            print('Bank created sucessfully!')
+
+        elif bank_op == '3':
+            for bank in BankManager().listBanks():
+                print(bank)
+                
+            del_bank = input('\nBank name: ')
+            x = input('Are you sure you want to delete this bank?\n(Y/N): ').upper()
+
+            if x == 'Y':
+                BankManager().delBank(del_bank)
+                print('Bank sucessfully deleted')
+            else:
+                print('Bank was not deleted')
+
+        elif bank_op == '4':
+            for bank in BankManager().listBanks():
+                print(bank)
+
+        elif bank_op == '5':
             break
-
-        else:
-            print('\nOption is not valid!')
 
 
 if __name__ == "__main__":
     mainLoop()
-
 
